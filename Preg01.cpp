509ae2601cb2150bd865b4140251d62c437f379f
rkmarkoz <marcos.saira.95@gmail.com>
2022-10-10 09:26:33 -0500

    Pregunta01 - Listo por SAIRA

---
+++ Preg01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a,b;
+	cout<<"Ingrese el primer valor  A:";
+	cin>>a;
+	cout<<"Ingrese el segundo valor  B:";
+	cin>>b;
+	
+	cout<<"El valor de A es: "<<b<<"\n";
+	cout<<"El valor de B es: "<<a;
+	
+	return 0;
+	
+}
